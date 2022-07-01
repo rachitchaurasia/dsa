e2e4e586ae8687c419fee24dcfc0c6819049960f
Rachit Chaurasia <rachitchaurasia2701@gmail.com>
2022-07-02 02:15:16 +0530

    Add if_else folder

---
+++ C++/if_else/vowelconsonant.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char c;
+    int islowercasevowel, isuppercasevowel ;
+    cout<<"Enter an alphabet- ";
+    cin>>c;
+    islowercasevowel = (c=='a' || c=='e'||c=='i'||c=='o'||c=='u');
+    isuppercasevowel = (c=='A' || c=='E'||c=='I'||c=='O'||c=='U');
+
+    if (islowercasevowel || isuppercasevowel)
+    {
+        cout<<"Vowel"<<endl;
+    }
+    else
+    {
+        cout<<"Consonant"<<endl;
+    }
+    return 0;
+}
