28e6076f54d94acbea301eaf32571897387c1ac6
Rachit Chaurasia <rachitchaurasia2701@gmail.com>
2023-12-24 01:16:25 +0530

    Added TypeCasting and TypePromotion java class

---
+++ Java/Basics/TypeCasting.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        float a = 12.34f;
+        //type casting
+        int b = (int)a;
+        System.out.println(b);
+        char ch = 'a';
+        //type conversion
+        int num = ch;
+        System.out.println(num); //ascii value
+    }
+}
---
+++ Java/Basics/TypePromotion.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class TypePromotion {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 14.65f;
+        long c = 25;
+        double d =30;
+        double ans = a+b+c+d;
+        System.out.println(ans);
+    }
+}
