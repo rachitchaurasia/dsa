baa7732377803ae2d47f03b3eb622fed1312312a
Rachit Chaurasia <rachitchaurasia2701@gmail.com>
2023-12-24 00:03:13 +0530

    Added Variables and DataTypes

---
+++ Java/Basics/DataTypes.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b = 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = true;
+        float price = 10.50f;
+        int n = 45;
+        //long
+        //double
+        short s = 240;
+    }
+}
---
+++ Java/Basics/Variables.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class Variables {
+    public static void main(String[] args) {
+        int a= 5;
+        int b= 10;
+        String name = "Rachit";
+        int perimeter = 2*(a+b);
+
+        System.out.println(perimeter);
+        System.out.println(name);
+    }
+}
